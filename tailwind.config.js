9e06868bdf61fc8d06354ba99527737d8cfc4d98
Durbek08 <durbekacer@gmail.com>
2024-02-08 17:58:58 +0300

    yangi

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "blog-grey-300":"#616161",
+        "blog-aqua-300":"#02A28F",
+      },
+      padding:{
+        "22":"22px",
+        "blog-12":"12px", 
+
+      }
+    },
   },
   plugins: [],
 }
